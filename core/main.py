c193cde96a3e0fca583547efc78ec1ed7bdd3c70
Nathan Papapietro <npapapietro95@gmail.com>
2022-09-20 14:05:48 -0500

    If release summary is too large, refer link (#3)
    * If release summary is too large, refer link
    
    * Bash weirdness
    
    * Changes

--- core/main.py
+++ core/main.py
@@ -19,8 +19,19 @@ APPLY_MARKDOWN = os.environ.get("APPLY_MARKDOWN")
 TF_BREAKLINE = r"─────────────────────────────────────────────────────────────────────────────"
 
 
-def parse_tf_json(file):
-    """Parses the terraform plan json output"""
+def parse_tf_json(file: str) -> str:
+    """This function reads the json output of the terraform plan and looks for actions that are
+    either 'create', 'delete' or 'update'. Read actions labelled as 'no-op' are ignored.
+
+    If there are multiple actions like 'create' and 'update' on a complex item such as module
+    or resource with lots of blocks, we apply the label 'mix'.
+
+    Args:
+        file (str): full path to the json terraform plan output.
+
+    Returns:
+        str: A markdown formatted table of columns ['address', 'action']
+    """
     with open(file) as f:
         data = json.load(f)
 
@@ -44,7 +55,7 @@ def parse_tf_json(file):
     return pd.DataFrame(records).to_markdown(index=False)
 
 
-def parse_tf_log(file):
+def parse_tf_log(file: str) -> str:
     """Parses the terraform plan log output"""
     with open(file) as f:
         raw = f.read()
@@ -52,8 +63,16 @@ def parse_tf_log(file):
     return raw
 
 
-def parse_tf_checkov(file):
-    """Parses the checkov scan json output"""
+def parse_tf_checkov(file: str) -> str:
+    """This functions reads the json output of the checkov run and selects only the needed
+    info from the result. Formats markdown with check_id links if existing.
+
+    Args:
+        file (str): full path to the json checkov scan output.
+
+    Returns:
+        str: A markdown formatted table of columns ['resource_address', 'check_id']
+    """
 
     with open(file) as f:
         data = json.load(f)
@@ -81,7 +100,16 @@ def parse_tf_checkov(file):
     return pd.DataFrame(records).to_markdown(index=False)
 
 
-def _read_apply_log(file):
+def _read_apply_log(file: str) -> pd.DataFrame:
+    """Parses the terraform apply log. The log is in jsonlines format,
+    so we use pandas DataFrame to read it for us.
+
+    Args:
+        file (str): file path to terraform apply output.
+
+    Returns:
+        pd.DataFrame: Dataframe of each apply action.
+    """
     try:
         # first attempt. Sometimes terraform likes to output stdout statements not in json format
         return pd.read_json(file, lines=True)
@@ -97,8 +125,17 @@ def _read_apply_log(file):
         return pd.DataFrame(lines)
 
 
-def parse_tf_apply(file):
-    """Parses the terraform apply json output"""
+def parse_tf_apply(file: str) -> str:
+    """This parases the terraform apply output and constructs a
+    markdown formatted table out of the results. We filter for the 'hook' key
+    in the output as address.
+
+    Args:
+        file (str): file path to terraform apply output.
+
+    Returns:
+        str: A markdown formatted table of columns ['address', 'message', 'type']
+    """
 
     data = _read_apply_log(file)
 
@@ -119,8 +156,8 @@ def parse_tf_apply(file):
     return df.loc[(df['address'].notna()) & (~df['type'].isin(ignores))].to_markdown(index=False)
 
 
-def parse_tf_apply_summary(file):
-    """Parses the terraform apply json output"""
+def parse_tf_apply_summary(file: str) -> str:
+    """Parses the terraform apply json output and returns the raw output."""
     data = pd.read_json(file, lines=True).to_dict('records')
 
     summary = [x["@message"] for x in data]
@@ -198,12 +235,27 @@ def github_plan_output(plan_txt, summary_txt, apply_txt, apply_summary):
     """
 
 
-def post_plan_output(summary):
+def post_apply_output(summary: str) -> str:
+    """Takes in the summary string formatted in markdown and 
+    attempts to post it to the release.
+
+    Args:
+        summary (str): Markdown formatted text.
+
+    Returns:
+        str: 'success' or 'failure'
+    """
+
     GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
     GITHUB_REPOSITORY_OWNER = os.environ["GITHUB_REPOSITORY_OWNER"]
     GITHUB_REPOSITORY_NAME = GITHUB_REPOSITORY.replace(GITHUB_REPOSITORY_OWNER + '/', '')
     GITHUB_REF_NAME = os.environ["GITHUB_REF_NAME"]
     GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+    GITHUB_RUN_ID = os.environ["GITHUB_RUN_ID"]
+
+    if len(summary) >= 125000:
+        os.environ["GITHUB_STEP_SUMMARY"] = os.environ.get("GITHUB_STEP_SUMMARY", "") + summary
+        summary = f"Release text too large, see plan summary from job at https://github.com/{GITHUB_REPOSITORY_NAME}/actions/runs/{GITHUB_RUN_ID}"
 
     response = requests.post(
         f"https://api.github.com/repos/{GITHUB_REPOSITORY}/releases",
@@ -228,61 +280,93 @@ def post_plan_output(summary):
     return "success"
 
 
-if __name__ == "__main__":
-
+def get_plan_text() -> str:
+    """Returns the plan text.
+    Wraps any errors for display."""
     try:
         if PLAN_CHECK != SUCCESS_ICON:
             raise Exception("Plan was not successful, see raw log.")
-        plan_txt = parse_tf_json(PLAN_PATH)
+        return parse_tf_json(PLAN_PATH)
     except Exception as e:
-        plan_txt = "Error loading plan: " + str(e)
+        return "Error loading plan: " + str(e)
 
+
+def get_summary_text() -> str:
+    """Returns the summary text.
+    Wraps any errors for display."""
     try:
-        summary_txt = parse_tf_log(LOG_PATH)
+        return parse_tf_log(LOG_PATH)
     except Exception as e:
-        summary_txt = "Error loading summary: " + str(e)
+        return "Error loading summary: " + str(e)
+
+
+def get_checkov_text() -> str:
+    """Returns the checkov text.
+    Wraps any errors for display."""
+    try:
+        if PLAN_CHECK != SUCCESS_ICON:
+            raise Exception("Plan was not successful, fix before running checov.")
+        return parse_tf_checkov(CHECKOV_PATH)
+    except Exception as e:
+        return "Error loading scan: " + str(e)
+
+
+def get_apply_text() -> tuple[str, str]:
+    """Returns the tuple of (apply text, summary apply text).
+    Wraps any errors for display."""
+    try:
+        apply_text = parse_tf_apply(APPLY_PATH)
+
+    except Exception as e:
+        apply_text = "Error loading apply. Reason: " + str(e)
+
+    # Even if tf apply failes, the output will still be in jsonl form.
+    try:
+        summary_apply_text = parse_tf_apply_summary(APPLY_PATH)
+    except Exception as e:
+        if os.path.isfile(APPLY_PATH):
+            with open(APPLY_PATH) as f:
+                summary_apply_text = f.read()
+        else:
+            summary_apply_text = "Error loading summary apply. Reason: No apply file found."
+
+    return (apply_text, summary_apply_text)
+
+
+def do_plan(plan_text: str, summary_text: str) -> None:
+    """Wraps all the logic for running this script in plan mode."""
+    checkov_text = get_checkov_text()
+
+    with open(APPLY_MARKDOWN, "w") as f:
+        f.write(github_summary_output(
+            FORMAT_CHECK,
+            INIT_CHECK,
+            PLAN_CHECK,
+            CHECKOV_CHECK,
+            plan_text,
+            summary_text,
+            checkov_text
+        ))
+
+
+def do_apply(plan_text: str, summary_text: str) -> None:
+    """Wraps all the logic for running this script in apply mode."""
+    (apply_text, summary_apply_text) = get_apply_text()
+
+    summary = github_plan_output(
+        plan_text, summary_text, apply_text, summary_apply_text
+    )
+    return_code = post_apply_output(summary)
+
+    print(f"::set-output name=applycheck::{return_code}")
+
+
+if __name__ == "__main__":
+    plan_text = get_plan_text()
+    summary_text = get_summary_text()
 
     if sys.argv[1] == "plan":
-
-        try:
-            if PLAN_CHECK != SUCCESS_ICON:
-                raise Exception("Plan was not successful, fix before running checov.")
-            checkov_txt = parse_tf_checkov(CHECKOV_PATH)
-        except Exception as e:
-            checkov_txt = "Error loading scan: " + str(e)
-
-        with open(APPLY_MARKDOWN, "w") as f:
-            f.write(github_summary_output(
-                FORMAT_CHECK,
-                INIT_CHECK,
-                PLAN_CHECK,
-                CHECKOV_CHECK,
-                plan_txt,
-                summary_txt,
-                checkov_txt
-            ))
+        do_plan(plan_text, summary_text)
 
     elif sys.argv[1] == "apply":
-
-        try:
-            apply_text = parse_tf_apply(APPLY_PATH)
-
-        except Exception as e:
-            apply_text = "Error loading apply. Reason: " + str(e)
-
-        # Even if tf apply failes, the output will still be in jsonl form.
-        try:
-            summary_apply_txt = parse_tf_apply_summary(APPLY_PATH)
-        except Exception as e:
-            if os.path.isfile(APPLY_PATH):
-                with open(APPLY_PATH) as f:
-                    summary_apply_txt = f.read()
-            else:
-                summary_apply_txt = "Error loading summary apply. Reason: No apply file found."
-
-        summary = github_plan_output(
-            plan_txt, summary_txt, apply_text, summary_apply_txt
-        )
-        return_code = post_plan_output(summary)
-
-        print(f"::set-output name=applycheck::{return_code}")
+        do_apply(plan_text, summary_text)
