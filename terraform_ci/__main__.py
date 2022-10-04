aa860bcac477fd79bd7e32b7ca6b2636835a5157
Nathan Papapietro <npapapietro95@gmail.com>
2022-10-04 17:07:18 -0500

    Changing directory

--- terraform_ci/__main__.py
+++ terraform_ci/__main__.py
@@ -1,9 +1,13 @@
+import os
+
 from .terraform import TfCLI
 from .pipeline import ActionPipeline
 from .config import Settings
 
 if __name__ == "__main__":
     settings = Settings().config
+
+    os.chdir(settings.working_directory)
 
     TfCLI.set_version(version=settings.terraform.version)
     TfCLI.set_token(host=settings.terraform.host, token=settings.terraform.token)
