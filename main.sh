a130454f66d962cb3b6af07bb155608624972d01
Nathan Papapietro <npapapietro95@gmail.com>
2022-10-05 09:50:41 -0500

    Return codes

--- main.sh
+++ main.sh
@@ -2,8 +2,12 @@
 
 python -m terraform_ci
 
+RETURN_CODE=$?
+
 input=/app/template_result.md
 while IFS= read -r line
 do
     echo "$line" >> $GITHUB_STEP_SUMMARY
 done < "$input"
+
+exit $RETURN_CODE
